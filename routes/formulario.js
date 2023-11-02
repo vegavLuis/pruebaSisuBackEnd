6cdc96fbad92e39f3c0ad72700bd7c395a34150a
cas25a <147539206+cas25a@users.noreply.github.com>
2023-11-02 16:36:51 -0600

    se hacen cambios

--- routes/formulario.js
+++ routes/formulario.js
@@ -1,23 +1,15 @@
 import express, { json } from "express";
-const router = express.Router()
+const router = express.Router();
 
 import formulario from "../models/formulario";
 
 // Enviamos los datos para guardar en la DB
-router.post('/save', async(req, res)=>{
-    const body = req.body
-    try {
-        const data = await formulario.create(body)
-        res.status(200).json()({
-            data: data,
-            mensaje: "Guardado con exito"
-        })
-    } catch (error) {
-        return res.status(500).json({
-            mensaje: "Ocurrio un error",
-            error
-        })
-    }
-})
+router.post("/save", async (req, res) => {
+  const body = req.body;
+  return res.status(200).json({
+    body: body,
+    mensaje: "Guardado con exito",
+  });
+});
 
-module.exports = router
+module.exports = router;
